69d35a91d837d421d77335950b53a6499127fa11
Babula Pradhan <babula.pradhan@nuvento.net>
2021-02-11 11:05:00 +0530

    Add project files.

---
+++ HttpClientExtensionsForTesting/TestHttpClientExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace System.Net.Http
+{
+    public static class TestHttpClientExtensions
+    {
+        public static Task<HttpResponseMessage> PostAsJsonAsync<TRequest>(this HttpClient client, string url, TRequest request)
+        {
+            var content = new StringContent(
+                              JsonSerializer.Serialize(request),
+                              Encoding.UTF8,
+                              "application/json");
+
+            return client.PostAsync(url, content);
+        }
+
+        public static async Task<TType> ReadAsTypeAsync<TType>(this HttpContent httpContent)
+        {
+            var stringContent = await httpContent.ReadAsStringAsync();
+            var typeData = JsonSerializer.Deserialize<TType>(stringContent);
+
+            return typeData ?? default;
+        }
+    }
+}
